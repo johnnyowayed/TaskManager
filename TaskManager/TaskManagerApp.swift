8e6a5cbd324301025bf864ace83018b3ce0fbd54
Johnny Owayed <johnny.owayed@gmail.com>
2025-03-07 10:02:33 +0400

    Initial Commit

---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by Johnny Owayed on 07/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
